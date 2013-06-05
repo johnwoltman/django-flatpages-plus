70fc6cc770e7b874b62ef600c791d99eb15747be
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-06-05 16:36:04 +0530

    Updated forms.py to add new function for admin form and add metadata css attributes

--- flatpages_plus/forms.py
+++ flatpages_plus/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from flatpages_plus.models import FlatPage
+from tinymce.models import HTMLField
 
 
 class FlatpageForm(forms.ModelForm):
@@ -10,6 +11,21 @@ class FlatpageForm(forms.ModelForm):
                       " and trailing slashes."),
         error_message = _("This value must contain only letters, numbers,"
                           " dots, underscores, dashes, slashes or tildes."))
+    
+    metadata = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'class': 'span6'}))
+    content = HTMLField()
+        
+    class Meta:
+        model = FlatPage
+        exclude = ['owner', 'sites', 'enable_comments', 'views', 'registration_required']
+        
+class FlatpageAdminForm(forms.ModelForm):
+    url = forms.RegexField(label=_("URL"), max_length=100, regex=r'^[-\w/\.~]+$',
+        help_text = _("Example: '/about/contact/'. Make sure to have leading"
+                      " and trailing slashes."),
+        error_message = _("This value must contain only letters, numbers,"
+                          " dots, underscores, dashes, slashes or tildes."))
 
     class Meta:
         model = FlatPage
+    
