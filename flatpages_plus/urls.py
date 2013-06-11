3e17378ac433d8af615fa27e77839546f96a2ee4
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-06-11 17:57:47 +0530

    Update urls.py to add confirm-delete and search configuration

--- flatpages_plus/urls.py
+++ flatpages_plus/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns('flatpages_plus.views',
     (r'^list/', 'list'),
     (r'^add/', 'add'),
     (r'^update/(?P<id>\d+)/$', 'update'),
+    (r'^delete/(?P<id>\d+)/$', 'delete'),
+    (r'^confirm-delete/(?P<id>\d+)/$', 'confirm_delete'),
+    (r'^search/$', 'search'),
     (r'^(?P<url>.*)$', 'flatpage'),
 )
