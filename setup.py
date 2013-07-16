87aa8a74b58ac615416b3f2c81a281a8b7285c48
shaijudavis <shaiju.davis@vanillanetworks.com>
2013-07-16 14:24:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-flatpages-plus',
-    packages=['flatpages_plus'],
-    include_package_data=True,
     version='0.1.1',
     description='A more robust FlatPage app for Django.',
     author='Dana Woodman',
     author_email='dana@danawoodman.com',
     url='https://github.com/danawoodman/django-flatpages-plus',
     license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
 )
